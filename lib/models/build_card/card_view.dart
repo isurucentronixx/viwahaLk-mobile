4097bb93409969d81ee6a1db37fabcb953b2e336
Thushan Isuru <101393829+isurucentronixx@users.noreply.github.com>
2023-05-30 23:40:51 +0530

    minor

--- lib/models/build_card/card_view.dart
+++ lib/models/build_card/card_view.dart
@@ -49,122 +49,122 @@ class CardView extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        GridView.count(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          crossAxisCount: 3, // Number of columns in the grid
-          children: [
-            _buildCard(
-              'Venues',
-              Assets.lib.assets.images.weddingVenues.image(),
-              () async {
-                AutoRouter.of(context).push(const VenuesRoute());
-              },
-            ),
-            _buildCard('Dressing', Assets.lib.assets.images.dressing.image(),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            crossAxisCount: 3, // Number of columns in the grid
+            children: [
+              _buildCard(
+                'Venues',
+                Assets.lib.assets.images.weddingVenues.image(),
+                () async {
+                  AutoRouter.of(context).push(const VenuesRoute());
+                },
+              ),
+              _buildCard('Dressing', Assets.lib.assets.images.dressing.image(),
+                  () {
+                AutoRouter.of(context).push(const DressingRoute());
+              }),
+              _buildCard(
+                'Jewelry',
+                Assets.lib.assets.images.jewelry.image(),
                 () {
-              AutoRouter.of(context).push(const DressingRoute());
-            }),
-            _buildCard(
-              'Jewelry',
-              Assets.lib.assets.images.jewelry.image(),
-              () {
-                // Action for when Card 3 is clicked
-              },
-            ),
-            _buildCard(
-              'Decorations',
-              Assets.lib.assets.images.decorations.image(),
-              () {
-                // Action for when Card 4 is clicked
-              },
-            ),
-            _buildCard(
-              'Photography',
-              Assets.lib.assets.images.photography.image(),
-              () {
-                // Action for when Card 5 is clicked
-              },
-            ),
-            _buildCard(
-              'Entertainment',
-              Assets.lib.assets.images.entertainment.image(),
-              () {
-                // Action for when Card 6 is clicked
-              },
-            ),
-            _buildCard(
-              'Salon',
-              Assets.lib.assets.images.makeup.image(),
-              () {
-                // Action for when Card 7 is clicked
-              },
-            ),
-            _buildCard(
-              'Food',
-              Assets.lib.assets.images.food.image(),
-              () {
-                // Action for when Card 8 is clicked
-              },
-            ),
-            _buildCard(
-              'Honeymoon',
-              Assets.lib.assets.images.honeymoon.image(),
-              () {
-                // Action for when Card 9 is clicked
-              },
-            ),
-            _buildCard(
-              'Wedding Car',
-              Assets.lib.assets.images.weddingCars.image(),
-              () {
-                // Action for when Card 10 is clicked
-              },
-            ),
-            _buildCard(
-              'Music',
-              Assets.lib.assets.images.music.image(),
-              () {
-                // Action for when Card 11 is clicked
-              },
-            ),
-            _buildCard(
-              'Others',
-              Assets.lib.assets.images.others.image(),
-              () {
-                // Action for when Card 12 is clicked
-              },
-            ),
-            // Add more cards as needed
-          ],
+                  // Action for when Card 3 is clicked
+                },
+              ),
+              _buildCard(
+                'Decorations',
+                Assets.lib.assets.images.decorations.image(),
+                () {
+                  // Action for when Card 4 is clicked
+                },
+              ),
+              _buildCard(
+                'Photography',
+                Assets.lib.assets.images.photography.image(),
+                () {
+                  // Action for when Card 5 is clicked
+                },
+              ),
+              _buildCard(
+                'Entertainment',
+                Assets.lib.assets.images.entertainment.image(),
+                () {
+                  // Action for when Card 6 is clicked
+                },
+              ),
+              _buildCard(
+                'Salon',
+                Assets.lib.assets.images.makeup.image(),
+                () {
+                  // Action for when Card 7 is clicked
+                },
+              ),
+              _buildCard(
+                'Food',
+                Assets.lib.assets.images.food.image(),
+                () {
+                  // Action for when Card 8 is clicked
+                },
+              ),
+              _buildCard(
+                'Honeymoon',
+                Assets.lib.assets.images.honeymoon.image(),
+                () {
+                  // Action for when Card 9 is clicked
+                },
+              ),
+              _buildCard(
+                'Wedding Car',
+                Assets.lib.assets.images.weddingCars.image(),
+                () {
+                  // Action for when Card 10 is clicked
+                },
+              ),
+              _buildCard(
+                'Music',
+                Assets.lib.assets.images.music.image(),
+                () {
+                  // Action for when Card 11 is clicked
+                },
+              ),
+              _buildCard(
+                'Others',
+                Assets.lib.assets.images.others.image(),
+                () {
+                  // Action for when Card 12 is clicked
+                },
+              ),
+              // Add more cards as needed
+            ],
+          ),
         ),
       ],
     );
   }
 
   Widget _buildCard(String title, imagePath, VoidCallback onTap) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: onTap,
-        child: Card(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 80,
-                child: imagePath,
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 80,
+              child: imagePath,
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
               ),
-              const SizedBox(height: 10.0),
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
